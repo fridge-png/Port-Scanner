294adb39ba6b7054c99b698515c48bdf5d0a9740
Youssef Darwich <58274928+fridge-png@users.noreply.github.com>
2023-10-06 14:11:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import socket
 from scapy.all import ARP, Ether, srp
 
 def scan_ip():
-    target_ip = "192.168.1.110/24"  # Replace with your network's subnet
+    target_ip = "192.168.1.120/24"  # Replace with your network's subnet
     arp = ARP(pdst=target_ip)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether / arp
@@ -16,23 +16,17 @@ def scan_ip():
 
 def scan_port(target_host, target_port):
     try:
-        # Create a socket object
         socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        # Set a timeout in seconds (adjust as needed)
         socket_obj.settimeout(1)
         
-        # Attempt to connect to the target host and port
         socket_obj.connect((target_host, target_port))
         
-        # If the connection succeeds, the port is open
         print(f"Port {target_port} is open")
         
-        # Close the socket
         socket_obj.close()
         
     except (socket.timeout, ConnectionRefusedError):
-        # If the connection times out or is refused, the port is closed
         print(f"Port {target_port} is closed")
 
 def main():
